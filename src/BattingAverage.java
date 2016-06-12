57430038d5f0456cbf0d1b315e84b833a2ecc856
Jonathan Abdo <jabdo79@gmail.com>
2016-06-12 18:12:40 -0400

    first commit

---
+++ src/BattingAverage.java
@@ -0,0 +1,13 @@
+/*Jonathan Abdo 6-10-16
+ * Calculates batting average and slugging percentage for one or more baseball players
+ */
+import java.util.Scanner;
+
+public class BattingAverage {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
